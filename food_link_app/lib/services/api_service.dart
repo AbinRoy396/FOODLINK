e1ad762e224f46adca3a79c88e7edbbc0568c6dd
Abin Roy <abinroy501@gmail.com>
2025-09-23 21:43:07 +0530

    push files

--- food_link_app/lib/services/api_service.dart
+++ food_link_app/lib/services/api_service.dart
@@ -7,7 +7,7 @@ import '/models/request_model.dart';
 import 'package:flutter/material.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://localhost:3000/api';
+  static const String baseUrl = "http://10.0.2.2:3000/api";
   static const String _tokenKey = 'auth_token';
 
   // Get stored token
